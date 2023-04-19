311841e545ae60a446707abc37e74af35df8fa79
Yesh Chandiramani <yesh0907@hotmail.com>
2023-04-19 13:06:13 -0700

    style popup

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,43 +1,29 @@
-import { useState } from 'react'
 import logo from '../media/logo.svg'
-import './App.css'
+import demo from '../media/demo.svg'
+import { Button } from 'flowbite-react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="text-center min-w-[350px]">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="min-w-[350px] font-sans">
+      <header className="min-h-[100vh] flex flex-col items-center p-2 gap-4">
+        <div className='flex items-center self-start'>
+          <img src={logo} className="h-10 w-10" alt="Reel.fyi logo" />
+          <h2 className='text-2xl font-semibold'>Reel.fyi</h2>
+        </div>
+        <a href="https://reel.fyi"
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex justify-center w-fit'>
+          <img src={demo} className='w-full' alt="Reel.fyi demo" />
+        </a>
+        <Button color="purple" className='w-11/12 mb-4'>
+          <a href="https://app.reel.fyi/dashboard"
+            target='_blank'
+            rel='noopener noreferrer'>
+            Dashboard
+          </a>
+        </Button>
       </header>
-      {/* <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p> */}
     </div>
   )
 }
