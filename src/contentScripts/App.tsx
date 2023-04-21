1fa492d1dfbe1a3d8e08a8929718678ffc71605d
Yesh Chandiramani <yesh0907@hotmail.com>
2023-04-21 14:18:40 -0700

    add popup and icon ui

--- src/contentScripts/App.tsx
+++ src/contentScripts/App.tsx
@@ -1,9 +1,59 @@
+import { useEffect, useState } from 'react';
+import logo from '../media/logo.svg';
+import { Button } from 'flowbite-react';
+import { AiOutlineClose } from 'react-icons/ai';
+
 function App() {
-    return (
-        <div>
-            <h1>Hello World</h1>
+  const [showPopup, setShowPopup] = useState(false);
+  // const [pos, setPos] = useState(0);
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     const par = document.querySelector('textarea')?.parentElement;
+  //     const grammarly = document.querySelector('grammarly-extension');
+  //     const root = document.querySelector('div#crx-root');
+  //     if (!par?.lastChild?.isEqualNode(root) || grammarly) {
+  //       setPos(10);
+  //     }
+  //   }
+  // }, []);
+
+  const popup = (
+    <div className="absolute -top-52 right-16 w-[28rem] bg-gray-100 rounded-lg shadow-lg z-50">
+      <div className="flex flex-col px-8 py-4 gap-4">
+        <div className='flex items-center justify-between self-start w-full'>
+          <div className='flex items-center'>
+            <img src={chrome.runtime.getURL(logo)} className="h-10 w-10" alt="Reel.fyi logo" />
+            <h2 className='text-2xl font-semibold'>Reel.fyi</h2>
+          </div>
+          <button className='text-2xl' onClick={() => setShowPopup(false)}>
+            <AiOutlineClose />
+          </button>
         </div>
-    )
+        <h4 className='text-xl font-light'>Your message:</h4>
+        <div className="bg-gray-200 rounded-lg p-3">
+          <p className='text-lg text-gray-700'>
+            Hi Zeyao, as an IU alum like yourself, I'm excited to connect with you. I'm Dake, a junior at IU and an incoming Microsoft PM intern. With my experience at a Techstars company, I'm interested in discussing product at Google and learning about your journey. Let's chat!
+          </p>
+        </div>
+        <div className='flex justify-between flex-grow mt-2 gap-x-8'>
+          <Button color="purple" fullSized>Copy Text</Button>
+          <Button color="purple" outline fullSized>Rewrite</Button>
+        </div>
+      </div>
+    </div>
+  )
+  return (
+    <div className='flex absolute bottom-7 right-0'>
+      <img
+        src={chrome.runtime.getURL(logo)}
+        alt="reel.fyi logo"
+        className='w-14'
+        onClick={() => setShowPopup(!showPopup)}
+      />
+      {showPopup && popup}
+    </div>
+  )
 }
 
 export default App;
--- src/contentScripts/index.tsx
+++ src/contentScripts/index.tsx
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../index.css'
 
-const root = document.createElement('div');
-root.id = "crx-root";
-document.body.appendChild(root);
+function mountApp() {
+  const root = document.createElement('div');
+  root.id = "crx-root";
+  document.querySelector('textarea')?.parentElement?.appendChild(root);
 
-ReactDOM.createRoot(root).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+const ADD_NOTE_BTN_QUERY = 'button[aria-label="Add a note"]';
+const addNoteBtn = document.querySelector(ADD_NOTE_BTN_QUERY);
+
+if (addNoteBtn) {
+  console.log('addNoteBtn found');
+  addNoteBtn.addEventListener('click', () => {
+    console.log("Add a note button clicked");
+    mountApp();
+  });
+} else {
+  const observer = new MutationObserver(() => {
+    const btn = document.querySelector(ADD_NOTE_BTN_QUERY);
+    if (btn) {
+      // figure out how to get icon to show up again after this observer disconnects
+      observer.disconnect();
+      btn.addEventListener('click', () => {
+        console.log("Add a note button clicked");
+        mountApp();
+      });
+    }
+  });
+  observer.observe(document.body, { childList: true, subtree: true });
+}
