b35d9e27c021ac79865300395c29e8457dcaede0
Yesh Chandiramani <yesh0907@hotmail.com>
2023-04-22 17:37:16 -0700

    progress on extension ui

--- src/contentScripts/App.tsx
+++ src/contentScripts/App.tsx
@@ -1,25 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import logo from '../media/logo.svg';
-import { Button } from 'flowbite-react';
+import { Badge, Button } from 'flowbite-react';
 import { AiOutlineClose } from 'react-icons/ai';
+import { clipboard } from '@extend-chrome/clipboard'
+
+const DEFAULT_MESSAGE = "Hi Zeyao, as an IU alum like yourself, I'm excited to connect with you. I'm Dake, a junior at IU and an incoming Microsoft PM intern. With my experience at a Techstars company, I'm interested in discussing product at Google and learning about your journey. Let's chat!"
 
 function App() {
   const [showPopup, setShowPopup] = useState(false);
-  // const [pos, setPos] = useState(0);
+  const [message, setMessage] = useState(DEFAULT_MESSAGE);
+  const [messageCopied, setMessageCopied] = useState(true);
 
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined') {
-  //     const par = document.querySelector('textarea')?.parentElement;
-  //     const grammarly = document.querySelector('grammarly-extension');
-  //     const root = document.querySelector('div#crx-root');
-  //     if (!par?.lastChild?.isEqualNode(root) || grammarly) {
-  //       setPos(10);
-  //     }
-  //   }
-  // }, []);
+  const copyText = async () => {
+    const copied = await clipboard.writeText(message);
+    if (copied === message) {
+      setMessageCopied(true);
+    }
+  }
+
+  const copyMessageBadge = (
+    <Badge color="success">
+      <span className="text-lg">
+        Copied!
+      </span>
+    </Badge>
+  )
 
   const popup = (
-    <div className="absolute -top-52 right-16 w-[28rem] bg-gray-100 rounded-lg shadow-lg z-50">
+    <div className="absolute -top-32 right-16 w-[28rem] bg-gray-100 rounded-lg shadow-lg z-50">
       <div className="flex flex-col px-8 py-4 gap-4">
         <div className='flex items-center justify-between self-start w-full'>
           <div className='flex items-center'>
@@ -30,21 +38,32 @@ function App() {
             <AiOutlineClose />
           </button>
         </div>
-        <h4 className='text-xl font-light'>Your message:</h4>
+        <div className='flex items-center justify-between'>
+          <h4 className='text-xl font-light'>Your message:</h4>
+          {messageCopied && copyMessageBadge}
+        </div>
         <div className="bg-gray-200 rounded-lg p-3">
           <p className='text-lg text-gray-700'>
             Hi Zeyao, as an IU alum like yourself, I'm excited to connect with you. I'm Dake, a junior at IU and an incoming Microsoft PM intern. With my experience at a Techstars company, I'm interested in discussing product at Google and learning about your journey. Let's chat!
           </p>
         </div>
         <div className='flex justify-between flex-grow mt-2 gap-x-8'>
-          <Button color="purple" fullSized>Copy Text</Button>
-          <Button color="purple" outline fullSized>Rewrite</Button>
+          <Button color="purple" fullSized>
+            <span className="flex items-center rounded-md text-lg px-6 py-1">
+              Copy Text
+            </span>
+          </Button>
+          <Button color="purple" outline fullSized>
+            <span className="flex items-center rounded-md text-lg px-6 py-1">
+              Rewrite
+            </span>
+          </Button>
         </div>
       </div>
     </div>
   )
   return (
-    <div className='flex absolute bottom-7 right-0'>
+    <div className='flex absolute top-0 -right-2'>
       <img
         src={chrome.runtime.getURL(logo)}
         alt="reel.fyi logo"
