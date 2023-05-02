7ac65edae75939d912fc86759f45398d20b7a19f
Yesh Chandiramani <yesh0907@hotmail.com>
2023-05-02 09:55:20 -0700

    turn background script into module for build

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,7 +1,10 @@
+// turn file into module
+export {};
+
 chrome.runtime.onInstalled.addListener(({ reason }) => {
   if (reason === "install") {
     chrome.tabs.create({
-      url: "https://app.reel.fyi/auth",
+      url: "https://app.reel.fyi/auth?ref=extension",
     });
   }
 });
