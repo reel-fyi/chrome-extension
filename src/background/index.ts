d97c9bcb656f156402f01922a0c14d788cdaac1f
Yesh Chandiramani <yesh0907@hotmail.com>
2023-05-01 19:51:01 -0700

    add install event listener to register user

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,19 +1,7 @@
-/*
-    DEAD CODE - NOT USED
-*/
-
-// relay savedUserData message to content script
-const relayMsg = async () => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    if (tab.id) {
-        await chrome.tabs.sendMessage(tab.id, {savedUserData: true});
-    }
-}
-
-// listen for webpage sending signal to get user data once saved
-chrome.runtime.onMessageExternal.addListener(async (message) => {
-    if (message.savedUserData) {
-        // relay message to content script
-        await relayMsg();
-    }
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === "install") {
+    chrome.tabs.create({
+      url: "https://app.reel.fyi/auth",
+    });
+  }
 });
